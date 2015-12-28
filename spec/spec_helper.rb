e1a2fe88262dd16762e1c42509b8f0472e39bfa9
rustaeja <rustaeja@ya.ru>
2015-12-28 23:14:16 +0300

    Update spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,11 +3,10 @@ require File.join(File.dirname(__FILE__), '..', 'app')
 require 'sinatra'
 require 'rack/test'
 require 'rspec'
-
-set :environment, :test
-set :run, false
-set :raise_errors, true
-set :logging, false
+                set :environment, :test
+                set :run, false
+                set :raise_errors, true
+                set :logging, false
 
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
 
