4b8807637f8d2c1dd4bc665fb2ca62045bc0000f
rustaeja <rustaeja@ya.ru>
2015-12-28 23:14:49 +0300

    Update app_spec.rb

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -4,14 +4,12 @@ describe 'Application' do
   def app
     Sinatra::Application
   end
-  
   describe 'index' do
     it 'should show the index page' do
       get '/'
       last_response.should be_ok
     end
   end
-
   describe 'the various layouts' do
     it 'should be able to show the different layouts' do
       %w(container-app fluid hero).each do |view|
